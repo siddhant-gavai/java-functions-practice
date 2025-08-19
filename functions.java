7e1f766196f597d0a44a9f4c949d3f3fcc9f27bc
Siddhant Gavai <siddhantgavai0@gmail.com>
2025-08-19 21:10:47 +0530

    Added Java functions example with Hello World and user-input based sum calculation

--- functions.java
+++ functions.java
@@ -1,19 +1,28 @@
+import java.util.Scanner;
+
 public class functions {
 
-    // Hello World method
     public static void helloWorld() {
         System.out.println("Hello World");
     }
 
-    // Sum method (do numbers ka sum nikalne ke liye)
-    public static void sum(int a, int b) {
+    public static void sum() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
         int result = a + b;
         System.out.println("Sum: " + result);
+
+        sc.close();
     }
 
     public static void main(String[] args) {
-        // Method call
-        helloWorld(); // Hello World print karega
-        sum(10, 20); // 10 + 20 = 30 print karega
+        helloWorld();
+        sum();
     }
 }
