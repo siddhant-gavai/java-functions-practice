688f199b8f528c970f25d5e81905fc37a3727d44
Siddhant Gavai <siddhantgavai0@gmail.com>
2025-08-21 18:28:43 +0530

    Add method overloading examples for add function with int

--- functionOverloading.java
+++ functionOverloading.java
@@ -9,10 +9,16 @@ public class functionOverloading {
         return a + b + c;
     }
 
+    public static double add(double a, double b) {
+        return a + b;
+    }
+
     public static void main(String[] args) {
         int sum1 = add(5, 10);
         int sum2 = add(5, 10, 15);
         System.out.println("Sum of two numbers: " + sum1);
         System.out.println("Sum of three numbers: " + sum2);
+        double sum3 = add(5.5, 10.5);
+        System.out.println("Sum of two double numbers: " + sum3);
     }
 }
