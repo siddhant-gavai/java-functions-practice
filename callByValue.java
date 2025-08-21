89762f14393bf847bce8c5e653a36858ea031623
Siddhant Gavai <siddhantgavai0@gmail.com>
2025-08-21 16:32:54 +0530

    some changes in callByValue

--- callByValue.java
+++ callByValue.java
@@ -3,15 +3,16 @@ public class callByValue {
         int temp = x;
         x = y;
         y = temp;
+        System.out.println("a = " + x + ", b = " + y);
+
     }
 
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
 
-        System.out.println("Before swap: a = " + a + ", b = " + b);
         swap(a, b);
-        System.out.println("After swap: a = " + a + ", b = " + b);
+
     }
 
 }
