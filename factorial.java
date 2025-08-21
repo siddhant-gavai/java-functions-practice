d24b99614a002ee243392b686200c43a96d70d13
Siddhant Gavai <siddhantgavai0@gmail.com>
2025-08-21 16:59:08 +0530

    Add two methods for factorial calculation: recursive and iterative

---
+++ factorial.java
@@ -0,0 +1,25 @@
+public class factorial {
+    public static int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+
+    public static int factorial2(int n) {
+        int f = 1;
+        for (int i = 2; i <= n; i++) {
+            f = f * i;
+        }
+        return f;
+    }
+
+    public static void main(String[] args) {
+        int number = 5;
+        int result = factorial(number);
+        System.out.println("Factorial of " + number + " = " + result);
+        int n = 10;
+        int result2 = factorial2(n);
+        System.out.println("Factorial of " + n + " = " + result2);
+    }
+}
