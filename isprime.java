3fc130cc898d369dd134a01728b64b8a767c3a56
Siddhant Gavai <siddhantgavai0@gmail.com>
2025-08-28 15:32:16 +0530

    Add isPrime method to check if a number

--- isprime.java
+++ isprime.java
@@ -1,5 +1,11 @@
 public class isprime {
+
+    // only for n >=2
     public static boolean isPrime(int n) {
+        if (n == 2) {
+            return true;
+
+        }
         for (int i = 2; i <= n - 1; i++) {
             if (n % i == 0) { // completly dividing
                 return false;
