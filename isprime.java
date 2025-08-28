35105376c2986374c1fa8b485305811fc0f96cb0
Siddhant Gavai <siddhantgavai0@gmail.com>
2025-08-28 17:55:22 +0530

    Add optimized isPrime method to check

--- isprime.java
+++ isprime.java
@@ -1,12 +1,27 @@
 public class isprime {
 
-    // only for n >=2
+    // // only for n >=2
+    // public static boolean isPrime(int n) {
+    // if (n == 2) {
+    // return true;
+
+    // }
+    // for (int i = 2; i <= n - 1; i++) {
+    // if (n % i == 0) { // completly dividing
+    // return false;
+    // }
+    // }
+    // return true;
+    // }
+
+    // Optimized way to write the number is prime or not
+
     public static boolean isPrime(int n) {
         if (n == 2) {
             return true;
 
         }
-        for (int i = 2; i <= n - 1; i++) {
+        for (int i = 2; i <= Math.sqrt(n); i++) {
             if (n % i == 0) { // completly dividing
                 return false;
             }
